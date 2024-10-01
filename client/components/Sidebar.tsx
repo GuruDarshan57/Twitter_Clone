caf845f1ff985bf6ed7c802ffb441c9f01ede353
Gurudarshan57 <dev560111@gmail.com>
2024-10-01 18:13:40 +0530

    refined sidebar ui

--- client/components/Sidebar.tsx
+++ client/components/Sidebar.tsx
@@ -8,36 +8,47 @@ import { TbAlignBoxLeftStretch } from "react-icons/tb";
 import { BsPeople } from "react-icons/bs";
 import { FiUser } from "react-icons/fi";
 import { CgMoreO } from "react-icons/cg";
+import { FaRegCircleUser } from "react-icons/fa6";
+import { IoIosMore } from "react-icons/io";
 import Link from 'next/link';
 
 interface Menuitem{
   name:string,
   icon:React.ReactNode;
+  functional:boolean
 }
 
 const sidebar_menu_items:Menuitem[] =[
-  {name: 'Home', icon:<GoHome/>},
-  {name: 'Explore', icon:<IoIosSearch/>},
-  {name: 'Notifications', icon:<HiOutlineBell />},
-  {name: 'Messages', icon:<HiOutlineEnvelope/>},
-  {name: 'Grok', icon:<TbAlignBoxLeftStretch />},
-  {name: 'Communities', icon:<BsPeople/>},
-  {name: 'Premium', icon:<FaXTwitter/>},
-  {name: 'Profile', icon:<FiUser/>},
-  {name:'More', icon:<CgMoreO/>}
+  {name: 'Home', icon:<GoHome/>, functional:true},
+  {name: 'Explore', icon:<IoIosSearch/>, functional:false},
+  {name: 'Notifications', icon:<HiOutlineBell />, functional:false},
+  {name: 'Messages', icon:<HiOutlineEnvelope/>, functional:false},
+  {name: 'Grok', icon:<TbAlignBoxLeftStretch />, functional:false},
+  {name: 'Communities', icon:<BsPeople/>, functional:false},
+  {name: 'Premium', icon:<FaXTwitter/>, functional:false},
+  {name: 'Profile', icon:<FiUser/>, functional:true},
+  {name:'More', icon:<CgMoreO/>, functional:true}
 ]
 
 const Sidebar = () => {
   return (
-    <div className='w-full h-full text-gray-300 '>
-        <div className="h-14 flex items-center justify-start px-2">
+    <div className='w-full h-full text-gray-200 py-2 flex flex-col justify-between'>
+        <div className="h-14 flex items-center justify-start px-1">
             <Link href="/home" className='text-3xl p-3 rounded-full hover:bg-gray-900 transition-all'><FaXTwitter/></Link>
         </div>
-        <div className="pt-1 flex flex-col">
-          {sidebar_menu_items.map(item=><div key={item.name} className="flex gap-3 w-fit rounded-full p-2 px-4 py-3 cursor-pointer text-3xl items-center justify-start hover:bg-gray-900">{item.icon}<span className='text-lg font-bold tracking-wider'>{item.name}</span></div>)}
+        <div className="flex flex-col gap-2">
+          {sidebar_menu_items.map(item=><div key={item.name} className={`flex gap-4 w-fit rounded-full p-2 px-4 py-2 text-2xl items-center justify-start hover:bg-gray-900 ${item.functional?"cursor-pointer":"cursor-not-allowed"}`}>{item.icon}<span className='text-lg tracking-wider'>{item.name}</span></div>)}
         </div>
-        <div className='flex w-full pt-2'>
-          <button className='p-2 mr-4 text-lg font-bold tracking-wider bg-sky-700 hover:bg-sky-800 w-full rounded-full'>Post</button>
+        <div className='flex w-full'>
+          <button className='p-3 mr-4 text-lg font-bold tracking-wider bg-sky-600 hover:bg-sky-700 w-full rounded-full'>Post</button>
+        </div>
+        <div className="flex gap-3 px-4 py-2 items-center justify-between rounded-full mr-4 hover:bg-gray-900">
+          <span className='text-4xl'><FaRegCircleUser/></span>
+          <div className="flex flex-col flex-1">
+            <p className='h-fit text-sm font-bold'>UserName</p>
+            <p className='h-fit text-xs'>user@145</p>
+          </div>
+          <span className='text-2xl cursor-pointer'><IoIosMore/></span>
         </div>
     </div>
   )
