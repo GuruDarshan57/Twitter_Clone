be5b92a33b64735c1a84f7bb17cfde8de5cbbce3
Gurudarshan57 <dev560111@gmail.com>
2024-11-02 17:38:48 +0530

    error handling

--- server/src/app/user/resolvers.ts
+++ server/src/app/user/resolvers.ts
@@ -4,35 +4,40 @@ import JWTservice from "../../services/JWT";
 
 const queries = {
   verifyGoogleToken: async (parent: any, { token }: { token: string }) => {
-    const googleToken = token;
-    const googleAuthURL = new URL("https://oauth2.googleapis.com/tokeninfo");
-    googleAuthURL.searchParams.append("id_token", googleToken);
-    const { data } = await axios.get(googleAuthURL.toString());
+    try {
+      const googleToken = token;
+      const googleAuthURL = new URL("https://oauth2.googleapis.com/tokeninfo");
+      googleAuthURL.searchParams.append("id_token", googleToken);
+      const { data } = await axios.get(googleAuthURL.toString());
 
-    const user = await prismaClient.user.findUnique({
-      where: { email: data.email },
-    });
-
-    if (!user) {
-      await prismaClient.user.create({
-        data: {
-          firstName: data.given_name,
-          lastName: data.family_name,
-          email: data.email,
-          profileImgUrl: data.picture,
-        },
+      const user = await prismaClient.user.findUnique({
+        where: { email: data.email },
       });
+
+      if (!user) {
+        await prismaClient.user.create({
+          data: {
+            firstName: data.given_name,
+            lastName: data.family_name,
+            email: data.email,
+            profileImgUrl: data.picture,
+          },
+        });
+      }
+
+      const userInDB = await prismaClient.user.findUnique({
+        where: { email: data.email },
+      });
+
+      if (!userInDB) throw new Error("User with email not found");
+
+      const userToken = JWTservice.createToken(userInDB);
+
+      return userToken;
+    } catch (error: Error | any) {
+      console.log(error.message);
+      return null;
     }
-
-    const userInDB = await prismaClient.user.findUnique({
-      where: { email: data.email },
-    });
-
-    if (!userInDB) throw new Error("User with email not found");
-
-    const userToken = JWTservice.createToken(userInDB);
-
-    return userToken;
   },
 };
 
