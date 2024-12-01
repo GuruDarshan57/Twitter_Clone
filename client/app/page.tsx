8d0355609525c3be8ab8d862437e66db6d7345c3
Gurudarshan57 <dev560111@gmail.com>
2024-12-01 09:07:01 +0530

    sigin/singnup ui changes

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -43,17 +43,17 @@ const Page = () => {
   }, []);
   return (
     <div className="flex flex-col w-full h-full items-center">
-      <div className="flex flex-col lg:flex-row w-full min-h-screen">
+      <div className="flex flex-col lg:flex-row w-full h-fit">
         <div className="flex justify-start lg:justify-center items-center pt-10 lg:pt-0 px-10 md:pl-20 lg:pl-0 lg:px-0 w-full lg:w-3/5">
           <FaXTwitter className="text-5xl lg:text-[330px] text-gray-100 relative lg:top-8 lg:right-5" />
         </div>
-        <div className="flex flex-col pt-16 pb-5 lg:pb-0 md:pt-24 md:pl-20 lg:pl-0 items-start w-full px-10 lg:px-0 lg:w-2/5">
+        <div className="flex flex-col pt-16 pb-5 lg:pb-5 md:pt-24 md:pl-20 lg:pl-0 items-start w-full px-10 lg:px-0 lg:w-2/5">
           <span className="text-5xl md:text-6xl font-extrabold pb-7 md:pb-10 tracking-wide relative -left-1">
             Happening now
           </span>
           <div className="w-full flex flex-col gap-2">
             <span className="text-3xl font-extrabold pb-7">Join today.</span>
-            <div className="text-black w-72 rounded-full flex justify-center items-center gap-2 font-bold cursor-pointer hover:bg-slate-100 overflow-hidden">
+            <div className="text-black w-full md:w-72 rounded-full flex justify-center items-center gap-2 font-bold cursor-pointer hover:bg-slate-100 overflow-hidden">
               <GoogleLogin
                 text={"signup_with"}
                 shape="pill"
@@ -66,21 +66,21 @@ const Page = () => {
                 useOneTap
               />
             </div>
-            <div className="bg-white w-72 text-black p-1 py-2 rounded-full flex justify-center items-center gap-2  font-bold cursor-not-allowed hover:bg-slate-100">
+            <div className="bg-white w-full md:w-72 text-black p-1 py-2 rounded-full flex justify-center items-center gap-2  font-bold cursor-not-allowed hover:bg-slate-100">
               <span className="text-xl">
                 <FaApple />
               </span>
               <span>Sign up with Apple</span>
             </div>
-            <div className="w-72 text-white p-1 py-2 flex justify-center items-center gap-2  font-bold cursor-not-allowed">
+            <div className="w-full md:w-72 text-white p-1 py-2 flex justify-center items-center gap-2  font-bold cursor-not-allowed">
               <div className="flex-1 border-slate-700 border-t-[0.5px] "></div>
               or
               <div className="flex-1 border-slate-700 border-t-[0.5px]"></div>
             </div>
-            <div className="bg-sky-500 hover:bg-sky-600 w-72 text-white p-1 py-2 rounded-full flex justify-center items-center gap-2  font-bold tracking-wider cursor-not-allowed">
+            <div className="bg-sky-500 hover:bg-sky-600 w-full md:w-72 text-white p-1 py-2 rounded-full flex justify-center items-center gap-2  font-bold tracking-wider cursor-not-allowed">
               Sign up with email or phone
             </div>
-            <div className=" w-80 p-1 py-2 rounded-full justify-center items-center gap-2 text-xs text-gray-600 cursor-not-allowed tracking-wide">
+            <div className=" w-full md:w-72 p-1 py-2 rounded-full justify-center items-center gap-2 text-xs text-gray-600 cursor-not-allowed tracking-wide">
               By signing up, you agree to the{" "}
               <span className="text-sky-600 hover:underline">
                 Terms of Service
@@ -92,10 +92,10 @@ const Page = () => {
               , including{" "}
               <span className="text-sky-600 hover:underline">Cookie Use</span>.
             </div>
-            <div className="w-72 text-white p-1 py-2 rounded-fullgap-2 mt-5  font-bold">
+            <div className="w-full md:w-72 text-white p-1 py-2 rounded-fullgap-2 mt-5  font-bold">
               Already have an account?
             </div>
-            <div className="w-72 text-sky-600 border-slate-700 border-[0.5px] p-1 py-2 rounded-full flex justify-center items-center gap-2  font-bold tracking-wider cursor-not-allowed">
+            <div className="w-full md:w-72 text-sky-600 border-slate-700 border-[0.5px] p-1 py-2 rounded-full flex justify-center items-center gap-2  font-bold tracking-wider cursor-not-allowed">
               Sign in
             </div>
           </div>
