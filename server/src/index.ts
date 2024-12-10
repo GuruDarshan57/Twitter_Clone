fee7ef730f9521f32926f85684ab6080528c5f4f
Gurudarshan57 <dev560111@gmail.com>
2024-12-10 23:05:12 +0530

    fix Port binding

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,7 +2,7 @@ import initilizeServer from "./app";
 
 async function init() {
   const app = await initilizeServer();
-  app.listen(8000, () => console.log("Server has started"));
+  app.listen(process.env.PORT || 8000, () => console.log("Server has started"));
   app.get("/", (req, res) => {
     res.send("<h1 style='color:green'>Server is up and running</h1>");
   });
